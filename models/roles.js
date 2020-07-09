5bd51190bd254f5fe4d45d7c5dfc5409bef04b7c
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-07-09 19:36:07 +0200

    Modificación del modelo de rol

--- models/roles.js
+++ models/roles.js
@@ -4,7 +4,7 @@ const sequelize = require('./db');
 const Rol = sequelize.define('rol', {
     id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
     nombre: {type: Sequelize.STRING, allowNull: false},
-    permisos: {type: Sequelize.STRING, allowNull: true}
+    permisos: {type: Sequelize.STRING(1000), allowNull: true}
 });
 
 module.exports = Rol;
