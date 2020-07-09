890a53cc701bb0ed511f411c9b1192c547e64d5e
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-07-09 19:31:12 +0200

    Corrección de fallo en modelo Usuario

--- models/usuarios.js
+++ models/usuarios.js
@@ -4,7 +4,7 @@ const sequelize = require('./db');
 const Usuario = sequelize.define('usuario', {
     id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
     nombre: {type: Sequelize.STRING, allowNull: false},
-    email: {type: Sequelize.STRING, unique: true, allowNull: true},
+    email: {type: Sequelize.STRING, unique: true, allowNull: false},
     password: {type: Sequelize.STRING, allowNull: false}
 });
 
