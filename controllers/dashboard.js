8b4bc83ca9665019fd06c646fe5dce6d83b981c2
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-07-14 19:05:56 +0200

    Formateo de fechas

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,4 +1,5 @@
 const { Usuario, Tarea } = require("../models");
+const moment = require("moment");
 
 function dashboard(req, res) {
     const usuario = req.session.usuario;
@@ -7,7 +8,14 @@ function dashboard(req, res) {
         include: {model: Tarea, as: 'tareas'}
     })
     .then(usuario => {
-        const tareas = usuario.tareas;
+        const tareas = usuario.tareas.map(tarea => {
+            return {
+                ...tarea,
+                fechaInicio: moment(tarea.fechaInicio).format("DD/MM/YYYY"),
+                fechaVencimiento: tarea.fechaVencimiento && moment(tarea.fechaVencimiento).format("DD/MM/YYYY")
+            }
+        });
+    
         res.render('dashboard', {usuario, tareas})
     })
     
