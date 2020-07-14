809a655a5c023cce24afcf4678161c891f431f9b
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-07-14 23:21:51 +0200

    Detalle de tarea

--- controllers/tareas.js
+++ controllers/tareas.js
@@ -1,6 +1,29 @@
+const { Tarea } = require("../models");
+const moment = require("moment")
+
 function mostrarTarea(req, res) {
     const id = req.params.id;
-    res.send("Tarea " + id);
+    Tarea.findByPk(id)
+    .then(tarea => {
+        // tarea.fechaInicio = moment(tarea.fechaInicio).format("DD/MM/YYYY")
+        // if (tarea.fechaVencimiento) {
+        //     tarea.fechaVencimiento = moment(tarea.fechaVencimiento).format("DD/MM/YYYY")
+        // }
+        // if (tarea.fechaFin) {
+        //     tarea.estado = "Finalizada"
+        //     tarea.fechaFin = moment(tarea.fechaFin).format("DD/MM/YYYY")
+        // } else {
+        //     tarea.estado = "Pendiente"
+        // }
+        res.render('tarea', {
+            tarea: {
+                estado: tarea.fechaFin? "Finalizado" : "Pendiente",
+                fechaInicio: moment(tarea.fechaInicio).format("DD/MM/YYYY"),
+                fechaFin: tarea.fechaFin && moment(tarea.fechaFin).format("DD/MM/YYYY"),
+                fechaVencimiento: tarea.fechaVencimiento && moment(tarea.fechaVencimiento).format("DD/MM/YYYY"),    
+            }
+        })
+    })
 }
 
 module.exports = {
