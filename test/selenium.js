ba4fdf766aafb2c2e69caf13f3f7e1f7f04be064
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-07-31 18:28:13 +0200

    Test de contraseña incorrecta

--- test/selenium.js
+++ test/selenium.js
@@ -19,11 +19,37 @@ describe("Formulario de login", function () {
             let firstResult = await driver.wait(until.elementLocated(By.css('h1')), 10000);
 
             //console.log(await firstResult.getAttribute('textContent'));
-            assert.equal("Bienvenido, Jairo", await firstResult.getAttribute('textContent'));
+            assert.equal("Bienvenido, Jairo", await (await firstResult.getAttribute('textContent')).trim());
 
         }
         finally{
-            driver.quit();
+            await driver.quit();
+        }
+
+    })
+
+
+    it("No inicia sesión con password incorrecta", async function () {
+        this.timeout(10000);
+
+        let driver = await new Builder().forBrowser('chrome').build();
+
+        try {
+            // Navigate to Url
+            await driver.get('http://localhost:3000/login');
+
+            // Introduce email, contraseña y pulsa enter
+            await driver.findElement(By.name('email')).sendKeys('jairochapela@gmail.com');
+            await driver.findElement(By.name('password')).sendKeys('adadasdadaf', Key.ENTER);
+
+            let firstResult = await driver.wait(until.elementLocated(By.css('div.alert.alert-danger')), 10000);
+
+            //console.log(await firstResult.getAttribute('textContent'));
+            assert.ok(firstResult);
+
+        }
+        finally{
+            await driver.quit();
         }
 
     })
